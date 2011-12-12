688b21967a07f14e31935e75e8f16fd58f67af6b
Michael Bleigh <michael@intridea.com>
2011-12-11 22:49:16 -0500

    Start stubbing it out, adds Guardfile

--- lib/omniauth/strategies/browser_id.rb
+++ lib/omniauth/strategies/browser_id.rb
@@ -1,11 +1,25 @@
 require 'omniauth'
+require 'omniauth-browserid'
 
 module OmniAuth
   module Strategies
     class BrowserID
       include OmniAuth::Strategy
 
+      option :verify_url, 'https://browserid.org/verify'
 
+      uid{ raw_info['email'] }
+
+      info do
+        {
+          :name => raw_info['email'],
+          :email => raw_info['email']
+        }
+      end
+
+      def raw_info
+
+      end
     end
   end
 end
